a8698400211a5303f2b745b53fab735614294ae9
supersourabh <kotagisourabh81@gmail.com>
2022-11-19 18:20:43 +0530

    v1.0.1

--- Backend/utils/sessionCheck.js
+++ Backend/utils/sessionCheck.js
@@ -4,7 +4,6 @@ var sessionCheck = (req, res, next) => {
 
     if (req.session.user && req.cookies.halls_user_auth) {
         var id = req.session.user
-        console.log(id);
         // req.db.query(`select * from bdhalls.users where id ='${id}';`, (err, response) => {
         //     if (err) res.render("html/error", { error: err.message, status: err.errno })
         //     else if (response.length == 0) {
